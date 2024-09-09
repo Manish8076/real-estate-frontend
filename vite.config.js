e6201a938da25b59865237408030bd98ee5b0b3f
Manish Singh <manishsingh77039@gmai.com>
2024-09-09 22:26:01 +0530

    again update

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server :{
     proxy : {
       '/api' : {
-        target : 'https://real-estate-backend-one-mu.vercel.app/',
+        target : 'https://real-estate-backend-one-mu.vercel.app',
         secure : false,
       },
     },
