13af1121c4353a6f502502501208afb2ae4c88c8
Manish Singh <manishsingh77039@gmai.com>
2024-09-09 22:42:53 +0530

    env update with backend url

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import { VITE_BACKEND_URL } from './.env'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server :{
     proxy : {
       '/api' : {
-        target : VITE_BACKEND_URL ,
+        target :  process.env.VITE_BACKEND_URL,
         secure : false,
       },
     },
