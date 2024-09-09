cc970a79834ccf1fb7a5802841cf1d878ddfa147
Manish Singh <manishsingh77039@gmai.com>
2024-09-09 22:35:14 +0530

    env again update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VITE_BACKEND_URL } from './.env'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server :{
     proxy : {
       '/api' : {
-        target : 'https://real-estate-backend-one-mu.vercel.app',
+        target : VITE_BACKEND_URL ,
         secure : false,
       },
     },
