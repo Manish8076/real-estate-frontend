5e1ba7310500e2a2b7a86736f5d3b4afee97c8f5
Manish Singh <manishsingh77039@gmai.com>
2024-09-12 13:05:25 +0530

    proxy remove

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   server :{
-    proxy : {
-      '/api' : {
-        target :  process.env.VITE_BACKEND_URL,
-        secure : false,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
-      },
-    },
+    
   },
   plugins: [react()],
 })
